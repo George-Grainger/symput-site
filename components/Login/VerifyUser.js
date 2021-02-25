c556394b19b8bd8f84be792e5835986b64194ab1
u87612gg <george.grainger-3@student.manchester.ac.uk>
2021-02-25 00:30:16 +0000

    Finished off login form for the time being

---
+++ components/Login/VerifyUser.js
@@ -0,0 +1,52 @@
+import { UserContext } from '@/lib/context';
+import { useContext, useEffect } from 'react';
+
+const VerifyUser = () => {
+  const { user, username, loading } = useContext(UserContext);
+
+  const sendEmail = async () => {
+    if (!loading) {
+      user
+        ?.sendEmailVerification()
+        .then(() => console.log('Success'))
+        .catch((error) => console.log(error));
+    }
+  };
+
+  useEffect(() => {
+    sendEmail();
+  }, [user]);
+
+  return (
+    <div className="grid md:grid-cols-5 w-full h-full">
+      <h1 className="md:col-span-5 text-3xl font-semibold w-full">
+        Please verify your email {username}
+      </h1>
+      <hr className="md:col-span-5 my-6 border-b-1 border-gray-200" />
+
+      <div className="md:col-span-6 grid mt-6 gap-y-6 max-w-prose mx-auto">
+        <h2 className="text-xl font-semibold">
+          If you signed up through Facebook, Twitter, GitHub or Google
+        </h2>
+        <p>
+          The link will be sent to the email address associated with the account
+          you signed in with.
+        </p>
+        <h2 className="text-xl font-semibold mt-12">
+          If you signed up through credentials
+        </h2>
+        <p>
+          The link will be sent to the email you registered the account with.
+        </p>
+      </div>
+      <button
+        onClick={sendEmail}
+        className="btn btn-yellow md:col-start-2 md:col-span-3 mt-12"
+      >
+        Resend the verification email
+      </button>
+    </div>
+  );
+};
+
+export default VerifyUser;
