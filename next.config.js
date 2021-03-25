ddcea7b1f6bba69856be9d5ba6f8d77db4f932a5
u87612gg <george.grainger-3@student.manchester.ac.uk>
2021-03-25 18:52:38 +0000

    Made app PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public'
+  },
   images: {
     domains: [
       'lh3.googleusercontent.com',
@@ -11,4 +17,4 @@ module.exports = {
     locales: ['en', 'zh-cn', 'ar'],
     defaultLocale: 'en'
   }
-};
+});
