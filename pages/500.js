9cabba793d3da643c09fb94ab5626c80c8aba70d
u87612gg <george.grainger-3@student.manchester.ac.uk>
2021-03-29 22:39:12 +0100

    Added 404 and 500 pages

---
+++ pages/500.js
@@ -0,0 +1,34 @@
+import { getFooterData, getNavbarData, getPageData } from '@/lib/pageContent';
+import Layout from 'layout/Layout';
+import Link from 'next/link';
+
+export const getStaticProps = ({ locale }) => {
+  const navbarData = getNavbarData(locale);
+  const footerData = getFooterData(locale);
+  const pageData = getPageData(locale, '500');
+
+  return {
+    props: {
+      navbarData,
+      footerData,
+      pageData
+    }
+  };
+};
+
+const Custom500 = ({ navbarData, footerData, pageData }) => {
+  const { title_i18n, paragraph_i18n, returnHome_i18n } = pageData;
+  return (
+    <Layout navbarData={navbarData} footerData={footerData}>
+      <section className="section-default section-default-padding justify-center dark:text-white">
+        <h1 className="text-6xl font-semibold mb-8">{title_i18n}</h1>
+        <p className="text-2xl mb-8">{paragraph_i18n}</p>
+        <Link href="/">
+          <a className="btn btn-black-inverted">{returnHome_i18n}</a>
+        </Link>
+      </section>
+    </Layout>
+  );
+};
+
+export default Custom500;
